88fd5f0c0a914558f5ae291ebc6f7cc8cd5a4cde
rickychan1987 <khaivanla1987@gmail.com>
2021-12-26 23:09:44 +0700

    Finish Project

---
+++ src/NoMatchPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NoMatchPage extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Page not found</h4>
+      </div>
+    );
+  }
+}
+
+export default NoMatchPage;
